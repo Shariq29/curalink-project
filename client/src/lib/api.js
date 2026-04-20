fcc3538656ec35544e0e032000c8b1ae08e71439
Shariq29 <shariqsayeed33@gmail.com>
2026-04-20 11:00:47 +0530

    Curalink project

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,32 +1,11 @@
 import axios from "axios";
 
-/* ------------------ AXIOS INSTANCE ------------------ */
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5001";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000",
-  timeout: 15000 // 15 sec timeout
+  baseURL: API_URL,
 });
-
-/* ------------------ REQUEST INTERCEPTOR ------------------ */
-api.interceptors.request.use(
-  (config) => {
-    // You can add auth token here later
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-/* ------------------ RESPONSE INTERCEPTOR ------------------ */
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("API Error:", error?.response || error.message);
-
-    return Promise.reject(
-      error?.response?.data || {
-        error: "Something went wrong while calling API"
-      }
-    );
-  }
-);
 
 export default api;
